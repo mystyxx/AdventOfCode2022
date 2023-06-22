ac21d29ce7e93a6f32bbbaa525872b53ea096509
Lya Roux <lya.roux@pm.me>
2023-06-22 16:08:51 +0200

    résolution du premier exercice

---
+++ 01/01.py
@@ -0,0 +1,24 @@
+number = []
+allpossiblenumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+sum = 0
+sumsum = []
+max = 0
+
+with open('doc.txt', 'r') as f:
+    lines = f.readlines()
+    calories = [entry.strip() for entry in lines]
+    for i in range(len(calories)):
+        if calories[i] != '':
+            sum = sum + int(calories[i])
+        else:
+            sumsum.append(sum)
+            sum=0
+
+    sumsum.append(sum)
+    i=+1
+    
+    for i in range(len(sumsum)):
+        if max < sumsum[i]:
+            max = sumsum[i]
+    print(max)
+
